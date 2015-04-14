b5f450c12e928d7ad7f65922ea7722c9a86b2ee1
Dominik93 <slusarz.dominik@gmail.com>
2015-04-14 21:28:22 +0200

    add maven

--- ModelMarketingowoHandlowy/src/main/java/com/slusarzwozniak/modelmarketingowohandlowy/ModelMarketingowoHandlowy.java
+++ ModelMarketingowoHandlowy/src/main/java/com/slusarzwozniak/modelmarketingowohandlowy/ModelMarketingowoHandlowy.java
@@ -1,15 +1,13 @@
-package com.slusarzwozniak.main;
-
-
-import com.slusarzwozniak.controller.Controller;
-import com.slusarzwozniak.model.Model;
-import com.slusarzwozniak.view.MainWindow;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.slusarzwozniak.modelmarketingowohandlowy;
+
+import com.slusarzwozniak.controller.Controller;
+import com.slusarzwozniak.model.Model;
+import com.slusarzwozniak.view.MainWindow;
 
 /**
  *
